99e55562560f8cabf6ab4180e8ab8da2a5194def
mamidi-srikar <101505609+mamidi-srikar@users.noreply.github.com>
2022-08-27 12:56:56 +0530

    Add files via upload
    NotesToDo

---
+++ app.js
@@ -0,0 +1,63 @@
+showNotes();
+
+let addbtn = document.getElementById('addbtn');
+addbtn.addEventListener("click", function (e) {
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    console.log(notesObj);
+    showNotes();
+
+})
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
+        <div class="card mx-2 my-3" style="width: 18rem;">
+           <div class="card-body">
+               <h5 class="card-title">Note ${index + 1}</h5>
+               <p class="card-text">${element}</p>
+               <button onclick="deleteNote(this.id)" id="${index}" class="btn btn-primary">Delete Note</button>
+           </div>
+        </div>
+           `;
+    });
+    let notesElm = document.getElementById("notes");
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
+    }
+    else {
+        notesElm.innerHTML = `There are no Notes to be displayed`;
+    }
+}
+
+
+function deleteNote(index) {
+    console.log("deleting the node",index);
+
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
+};
